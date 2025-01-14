6d118db488f50c4a91d82d9862b9eb214692464b
AZeKyoo <victor.gaspard.mail@gmail.com>
2025-01-14 23:28:26 +0100

    Job 2 | Fini  +  Job 3 | Fini

---
+++ p-collaborative-filtering-job-3/src/main/java/org/epf/hadoop/colfil3/RecommendationReducer.java
@@ -0,0 +1,51 @@
+package org.epf.hadoop.colfil3;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+import java.util.*;
+
+public class RecommendationReducer extends Reducer<Text, UserRecommendation, Text, Text> {
+    private static final int TOP_N = 5;
+    private Text result = new Text();
+
+    @Override
+    protected void reduce(Text key, Iterable<UserRecommendation> values, Context context)
+            throws IOException, InterruptedException {
+        // TreeSet pour garder automatiquement les TOP_N recommandations triées
+        TreeSet<UserRecommendation> topRecommendations = new TreeSet<>((a, b) -> {
+            // D'abord trier par nombre de relations communes (décroissant)
+            int compare = -Integer.compare(a.getCommonFriends(), b.getCommonFriends());
+            if (compare != 0) return compare;
+
+            // En cas d'égalité, trier par ID d'utilisateur
+            return a.getRecommendedId().compareTo(b.getRecommendedId());
+        });
+
+        // Collecter toutes les recommandations
+        for (UserRecommendation val : values) {
+            UserRecommendation copy = new UserRecommendation(
+                    val.getUserId(),
+                    val.getRecommendedId(),
+                    val.getCommonFriends()
+            );
+            topRecommendations.add(copy);
+
+            // Ne garder que les TOP_N meilleures recommandations
+            if (topRecommendations.size() > TOP_N) {
+                topRecommendations.pollLast();
+            }
+        }
+
+        // Construire la liste des recommandations
+        if (!topRecommendations.isEmpty()) {
+            List<String> recommendations = new ArrayList<>();
+            for (UserRecommendation rec : topRecommendations) {
+                recommendations.add(String.format("%s(%d)",
+                        rec.getRecommendedId(), rec.getCommonFriends()));
+            }
+            result.set(String.join(", ", recommendations));
+            context.write(key, result);
+        }
+    }
+}
---
+++ p-collaborative-filtering-job-3/src/main/java/org/epf/hadoop/colfil3/UserRecommendation.java
@@ -0,0 +1,60 @@
+package org.epf.hadoop.colfil3;
+
+import org.apache.hadoop.io.WritableComparable;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class UserRecommendation implements WritableComparable<UserRecommendation> {
+    private String userId;         // Utilisateur pour qui on fait la recommandation
+    private String recommendedId;  // Utilisateur recommandé
+    private int commonFriends;    // Nombre d'amis en commun
+
+    // Constructeur par défaut requis par Hadoop
+    public UserRecommendation() {
+        this.userId = "";
+        this.recommendedId = "";
+        this.commonFriends = 0;
+    }
+
+    // Constructeur avec paramètres
+    public UserRecommendation(String userId, String recommendedId, int commonFriends) {
+        this.userId = userId;
+        this.recommendedId = recommendedId;
+        this.commonFriends = commonFriends;
+    }
+
+    // Getters
+    public String getUserId() { return userId; }
+    public String getRecommendedId() { return recommendedId; }
+    public int getCommonFriends() { return commonFriends; }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeUTF(userId);
+        out.writeUTF(recommendedId);
+        out.writeInt(commonFriends);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        userId = in.readUTF();
+        recommendedId = in.readUTF();
+        commonFriends = in.readInt();
+    }
+
+    @Override
+    public int compareTo(UserRecommendation o) {
+        // Trier d'abord par nombre d'amis communs (décroissant)
+        int compare = -Integer.compare(this.commonFriends, o.commonFriends);
+        if (compare != 0) return compare;
+
+        // En cas d'égalité, trier par ID de l'utilisateur recommandé
+        return this.recommendedId.compareTo(o.recommendedId);
+    }
+
+    @Override
+    public String toString() {
+        return recommendedId + "(" + commonFriends + ")";
+    }
+}
