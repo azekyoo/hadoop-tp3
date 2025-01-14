5a425c725c610315e9f809e36ca3ad01bdc850d7
AZeKyoo <victor.gaspard.mail@gmail.com>
2025-01-14 22:58:08 +0100

    Job 2 | Classes

---
+++ p-collaborative-filtering-job-2/src/main/java/org/epf/hadoop/colfil2/CommonRelationshipReducer.java
@@ -0,0 +1,38 @@
+package org.epf.hadoop.colfil2;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+
+public class CommonRelationshipReducer
+        extends Reducer<UserPair, IntWritable, Text, IntWritable> {
+
+    private Text outputKey = new Text();
+    private IntWritable result = new IntWritable();
+
+    @Override
+    protected void reduce(UserPair key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+        int sum = 0;
+        boolean areDirectFriends = false;
+
+        // Parcourir toutes les valeurs
+        for (IntWritable val : values) {
+            if (val.get() == -1) {
+                // S'ils sont déjà amis, on ne veut pas les voir dans les résultats
+                areDirectFriends = true;
+                break;
+            }
+            sum += val.get();
+        }
+
+        // Émettre seulement s'ils ne sont pas amis directs et ont des amis en commun
+        if (!areDirectFriends && sum > 0) {
+            outputKey.set(key.toString());
+            result.set(sum);
+            context.write(outputKey, result);
+        }
+    }
+}
