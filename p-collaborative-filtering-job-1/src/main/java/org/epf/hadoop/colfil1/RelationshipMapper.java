a6f3cd0ae47d8bbf7e22e35147d882c3f1f37f61
AZeKyoo <victor.gaspard.mail@gmail.com>
2025-01-14 16:15:21 +0100

    Job 1 | Fini

--- p-collaborative-filtering-job-1/src/main/java/org/epf/hadoop/colfil1/RelationshipMapper.java
+++ p-collaborative-filtering-job-1/src/main/java/org/epf/hadoop/colfil1/RelationshipMapper.java
@@ -6,24 +6,13 @@ import org.apache.hadoop.io.LongWritable;
 
 import java.io.IOException;
 
-public class RelationshipMapper extends Mapper<LongWritable, Text, Text, Text> {
+public class RelationshipMapper extends Mapper<LongWritable, Relationship, Text, Text> {
 
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        // Lire la ligne et la convertir en Relationship
-        String line = value.toString();
-        String[] fields = line.split(",");
+    protected void map(LongWritable key, Relationship value, Context context) throws IOException, InterruptedException {
 
-        if (fields.length == 3) {
-            // Extraire les informations de la relation
-            String user1 = fields[0].trim();
-            String user2 = fields[1].trim();
-            String relationshipType = fields[2].trim();
+            context.write(new Text(value.getId1()), new Text(value.getId2()));
+            context.write(new Text(value.getId2()), new Text(value.getId1()));
 
-            // Créer deux paires (user1, user2) et (user2, user1)
-            // La clé est l'utilisateur, la valeur est l'utilisateur lié.
-            context.write(new Text(user1), new Text(user2));
-            context.write(new Text(user2), new Text(user1));
-        }
     }
 }
